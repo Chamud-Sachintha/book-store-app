41aeac5e6a86e240215358ed4c3e0f63a538bcd1
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-09-02 21:08:05 +0530

    initialize android project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,12 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    PrivacyScreen: {
+      enable: true,
+    },
+  },
 };
 
 export default config;

c85b5602c6a9e5ade5f3cd88c5369d10c75002ac
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-09-08 12:53:00 +0530

    add star rating for book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PdfViewerModule } from 'ng2-pdf-viewer'
+import { NgxStarRatingModule } from 'ngx-star-rating';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PdfViewerModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PdfViewerModule, NgxStarRatingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/modules/web-module/inside-book/inside-book.component.ts
+++ src/app/modules/web-module/inside-book/inside-book.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { NgxStarRatingModule } from 'ngx-star-rating';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inside-book',
   templateUrl: './inside-book.component.html',
   styleUrls: ['./inside-book.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule ,CommonModule, NgxStarRatingModule, FormsModule, ReactiveFormsModule]
 })
 export class InsideBookComponent  implements OnInit {
 
-  constructor(private router: Router) { }
+  startRating!: number;
+  public ratingForm!: FormGroup;
 
-  ngOnInit() {}
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.initRatingForm();
+  }
+
+  initRatingForm() {
+    this.ratingForm = this.formBuilder.group({
+      rating: ['', Validators.required]
+    })
+  }
 
   onClickProfileSection() {
     this.router.navigate(['/profile']);

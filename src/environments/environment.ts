6977600e0ea385da34c1ca40b1256662da7ae731
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-09-27 13:10:09 +0530

    implement firebase analytics

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,17 @@ export const environment = {
   production: false,
   // appUrl: "https://api.dpuremaths.lk/api/",
   fileServer: "https://admin.dpuremaths.lk/api/",
-  appUrl: "http://localhost:8000/api/"
+  appUrl: "http://localhost:8000/api/",
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBv7xWLtm9jPfsr64A7JirYRgfFJ1C7XpU",
+    authDomain: "dpuremathslk-b9358.firebaseapp.com",
+    projectId: "dpuremathslk-b9358",
+    storageBucket: "dpuremathslk-b9358.appspot.com",
+    messagingSenderId: "169973751650",
+    appId: "1:169973751650:web:789ba470251c3ac6e8163b",
+    measurementId: "G-YR1KPX56PH"
+  }
 };
 
 /*

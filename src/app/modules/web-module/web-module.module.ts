39aab8025d004895036682616ec7b5cf1f3b5836
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-10-17 11:13:39 +0530

    new changes

--- src/app/modules/web-module/web-module.module.ts
+++ src/app/modules/web-module/web-module.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { WebModuleRoutingModule } from './web-module-routing.module';
 import { HomeComponent } from './home/home.component';
-
+ 
 
 @NgModule({
   declarations: [
